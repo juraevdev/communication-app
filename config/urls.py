63b99484c4439dad57a3dbea7ab60c8901b1fcd2
davlat <juraevdevpy@gmail.com>
2025-09-30 10:50:16 +0500

    stuff

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from rest_framework import permissions
+from rest_framework import permissions 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -39,6 +39,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns += i18n_patterns(
     path('set_language/', include('django.conf.urls.i18n')),
