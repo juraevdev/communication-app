59c5860d603d9ee88dbf62d906ace1ae3d3d331b
davlat <juraevdevpy@gmail.com>
2025-09-30 10:41:20 +0500

    stuff

--- config/packages/channels.py
+++ config/packages/channels.py
@@ -9,7 +9,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('172.16.8.92:8000', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
