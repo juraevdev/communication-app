38092535d9cd021f056d1813a8f5285a08a1e10b
davlat <juraevdevpy@gmail.com>
2025-10-02 11:35:06 +0500

    stuff

--- groups/views.py
+++ groups/views.py
@@ -207,9 +207,9 @@ class LeaveGroup(generics.GenericAPIView):
         except Group.DoesNotExist:
             return Response({"detail": "Group not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        if request.user in group.members.all():
-            group.members.remove(request.user)
+        membership = GroupMember.objects.filter(group=group, user=request.user).first()
+        if membership:
+            membership.delete()
             return Response({"detail": "Successfully left the group."}, status=status.HTTP_200_OK)
 
-        return Response({"detail": "You are not a member of this group."}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response({"detail": "You are not a member of this group."}, status=status.HTTP_400_BAD_REQUEST)    
